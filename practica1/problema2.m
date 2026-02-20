273f5bdff1bd814d85716dd7adf689524e6c01ae
MohamedTayem <74103996+MohamedTayem@users.noreply.github.com>
2026-02-20 12:17:26 +0100

    problema 2 hecho

--- practica1/problema2.m
+++ practica1/problema2.m
@@ -1,7 +1,5 @@
-clear;
-clc;
 
-im = imread('fotografia/practica1/imagen.png');
+im = imread('fotografia-computacional/practica1/imagen.png');
 im = im2double(im);
 
 R = rand(size(im));
@@ -23,7 +21,7 @@ for a=1:2
     M = M1;
 end
 
-R3 = M + 0.5 * del
+R3 = M + 0.5 * del;
 
 [h,w] = size(im);
 
@@ -34,4 +32,37 @@ im2 = (im >= RA);
 imshow(im2);
 %imshow(im2(110:220,320:440,:));
 
-err = std2(im2-im)
+err = std2(im2-im);
+
+R1 = 0 + (1-0) * rand(128);
+
+B1 = repmat(B,2,2);
+
+imshow([R1 B1]);
+
+G = fspecial('gauss', 7, 1.5);
+R1_filt = imfilter(R1, G, 'symm');
+B1_filt = imfilter(B1, G, 'symm');
+
+imshow([R1_filt B1_filt]);
+
+B2 = repmat(B,8,8);
+
+im_B = (im >= B2);
+
+imshow(im_B);
+
+
+im_color = imread('fotografia-computacional/practica1/color.jpg');
+
+R_dith = im_color(:,:,1) >= B2;
+G_dith = im_color(:,:,2) >= B2;
+B_dith = im_color(:,:,3) >= B2;
+
+im_dith = cat(3, R_dith, G_dith, B_dith);
+
+im_c2 = im2double(im_dith);
+
+imshow(im_c2);
+
+
