29474a07b8e54291e0de3b12d48f7bad841bb1fa
Samuel Caraballo Chichiraldi (sammas24) <75754683+SamC4r@users.noreply.github.com>
2026-05-18 17:14:15 +0200

    Update ej1.m

--- practica6/ej1.m
+++ practica6/ej1.m
@@ -45,8 +45,9 @@ function Zdata=sample_hdr(hdr_data,n)
    Zdata = ZZ;
 end
 
-Zdata = sample_hdr(hdr_data, 8);
 Zdata = sample_hdr(hdr_data, 16);
+Zdata = sample_hdr(hdr_data, 8);
+
 
 figure;
 plot(Zdata(10,:));
@@ -54,3 +55,80 @@ hold on;
 plot(Zdata(140,:));
 plot(Zdata(1700,:));
 plot(Zdata(1565,:));
+
+
+function g = solve_G(Zdata,T)
+    [N,P] = size(Zdata);
+    Neq = 254 + N*(P - 1)
+    b = zeros(Neq,1);
+    
+    no_nulos = 2*N*(P - 1) + 3*254
+    i = zeros(1,no_nulos);
+    j = zeros(1,no_nulos);
+    v = zeros(1,no_nulos);
+
+    per = (100 * no_nulos) / (Neq * 256);
+    
+    eqs = 1;
+    bruh = 1;
+    for k=1:N
+        Z = Zdata(k,:);
+        [val,pos] = min(abs(Z - 128));
+        Zref = Z(pos);
+        Tref = T(pos);
+
+        for t=1:P
+            if t ~= pos
+
+                Zk      = Z(t); 
+                Tk      = T(t);
+
+                i(bruh) = eqs;
+                j(bruh) = Zk; 
+                v(bruh) = 1;
+                
+                bruh = bruh + 1;
+    
+                i(bruh) = eqs;
+                j(bruh) = Zref;
+                v(bruh) = -1;
+
+                b(eqs) = log2(Tk/Tref);
+
+                bruh = bruh + 1;
+                eqs = eqs + 1;
+            end
+        end
+    end
+
+    for gg=1:254
+        i(bruh) = eqs;
+        j(bruh) = gg;
+        v(bruh) = -1;
+
+        bruh = bruh + 1;
+
+        i(bruh) = eqs;
+        j(bruh) = gg + 1; 
+        v(bruh) = 2;
+       
+        bruh = bruh + 1;
+        
+        i(bruh) = eqs;
+        j(bruh) = gg + 2; 
+        v(bruh) = -1;
+
+        bruh = bruh + 1;
+        eqs = eqs + 1;
+        
+    end
+    
+    H = sparse(i,j,v,Neq,256);
+    whos H;
+    ss = sum(H ~= 0);
+    plot(ss)
+    g10 = ss(10)
+    g20 = ss(20)
+end
+
+solve_G(Zdata,T)
